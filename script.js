89dfd2f4a9a2cb5f8e1fe1603dd2bc4c9df6b89f
tanishamanickavelan <tanisha.manickavelan@gmail.com>
2024-06-24 13:47:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let startTime, elapsedTime = 0, timerInterval;
+const display = document.getElementById('display');
+const startStopBtn = document.getElementById('startStopBtn');
+const resetBtn = document.getElementById('resetBtn');
+const laps = document.getElementById('laps');
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 1000;
+    let ms = Math.floor(diffInMs);
+
+    let formattedHH = hh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+
+    return `${formattedHH}:${formattedMM}:${formattedSS}`;
+}
+
+function print(txt) {
+    display.innerHTML = txt;
+}
+
+function start() {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(function printTime() {
+        elapsedTime = Date.now() - startTime;
+        print(timeToString(elapsedTime));
+    }, 10);
+    showButton("PAUSE");
+}
+
+function pause() {
+    clearInterval(timerInterval);
+    showButton("PLAY");
+}
+
+function reset() {
+    clearInterval(timerInterval);
+    print("00:00:00");
+    elapsedTime = 0;
+    laps.innerHTML = '';
+    showButton("PLAY");
+}
+
+function showButton(buttonKey) {
+    const button = startStopBtn;
+    if (buttonKey === "PLAY") {
+        button.innerHTML = "Start";
+        button.onclick = start;
+    } else {
+        button.innerHTML = "Pause";
+        button.onclick = pause;
+    }
+}
+
+startStopBtn.addEventListener("click", start);
+resetBtn.addEventListener("click", reset);
